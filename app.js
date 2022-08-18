6082f5685e4a6a89cbd94222bd6ef3a63ad76372
j-leklere <jleklere@yahoo.com.ar>
2022-08-18 18:43:04 -0300

    agregando express y nodemon

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const publicPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicPath));
+
+app.listen(300, () => {
+  console.log("Servidor corriendo en el puerto 3000");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "./views/index.html"));
+});
