86aab9fec37609409a882d084ab24ecba42493ad
JeanGoP <jperezperalta@gmail.com>
2022-08-30 16:42:25 -0500

    Cambio de nombre de pagina de Productdetal a product

--- app.js
+++ app.js
@@ -6,25 +6,25 @@ const publicPath = path.resolve(__dirname, "./public");
 app.use(express.static(publicPath));
 
 app.listen(3000, () => {
-  console.log("Servidor corriendo en el puerto 3000");
+    console.log("Servidor corriendo en el puerto 3000");
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/index.html"));
+    res.sendFile(path.resolve(__dirname, "./views/index.html"));
 });
 
-app.get("/productDetail", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/productDetail.html"));
+app.get("/product", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./views/product.html"));
 });
 
 app.get("/login", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/login.html"));
+    res.sendFile(path.resolve(__dirname, "./views/login.html"));
 });
 
 app.get("/register", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/register.html"));
+    res.sendFile(path.resolve(__dirname, "./views/register.html"));
 });
 
 app.get("/productCart", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./views/productCart.html"));
+    res.sendFile(path.resolve(__dirname, "./views/productCart.html"));
 });
