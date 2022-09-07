6fddfa328bdb2cbd015dc7837e735dcd2d999808
javierjazmin <javierjazmin@gmail.com>
2022-09-07 13:23:52 -0300

    borre rutas viejas

--- app.js
+++ app.js
@@ -26,31 +26,6 @@ app.listen(3000, () => {
     console.log("Servidor corriendo en el puerto 3000");
 });
 
-
-app.get("/product", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/product.html"));
-});
-
-app.get("/productDetail", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/productDetail.html"));
-});
-
-app.get("/productFutbol", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/productFutbol.html"));
-});
-
-/*app.get("/login", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/login.html"));
-});
-
-app.get("/register", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/register.html"));
-});
-*/
-app.get("/productCart", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./views/productCart.html"));
-});
-
 // Para Heroku
 app.listen(process.env.PORT || 3030, function() {
     console.log("Puerto funcionando en el 3030!");
