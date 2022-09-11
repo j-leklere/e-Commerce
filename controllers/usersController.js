5445943e813539b0988cf4aded81c0836a0ecf85
JeanGoP <jperezperalta@gmail.com>
2022-09-10 20:00:01 -0500

    Crear arreglo de tallas y agregarlo al select

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -17,16 +17,42 @@ let categorias = [
     }
 ];
 
+let tallas = [
+    {
+        id: 1,
+        numero: 7
+    },
+    {
+        id: 2,
+        numero: 8
+    },
+    {
+        id: 3,
+        numero: 9.5
+    },
+    {
+        id: 1,
+        numero: 10
+    }
+];
+
+let usuarios = [
+    {
+        login: "jperezperalta@gmail.com",
+        password: 12345
+    }
+]
+
 const usersController = {
 
     login: (req,res) => {
-        res.render('../views/users/login')
+        res.render('../views/users/login',{usuarios: usuarios})
     },
     register: (req,res) => {
         res.render('../views/users/register')
     },
     crearEditar: (req,res) => {
-        res.render('../views/users/crearEditar.ejs',{categorias: categorias})
+        res.render('../views/users/crearEditar.ejs',{categorias: categorias, tallas: tallas})
     }
 };
 
