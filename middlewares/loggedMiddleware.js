2f396bd595d4fcff047c503876a4d7b737efc061
j-leklere <jleklere@yahoo.com.ar>
2022-10-31 18:54:40 -0300

    middlewares auth y logged

--- middlewares/loggedMiddleware.js
+++ middlewares/loggedMiddleware.js
@@ -1,6 +1,6 @@
 function loggedMiddleware(req, res, next) {
-  if (req.session.userLogged) {
-    return res.redirect("/products");
+  if (req.session.usuarioLogueado) {
+    return res.redirect("/");
   }
   next();
 }
