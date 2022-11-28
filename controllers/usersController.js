707c2724ad0b7e516f51cf817b1e349041df22d1
Valentinaadle <valentinaadle1@gmail.com>
2022-11-28 15:03:48 -0300

    hice crud de users

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,6 +2,10 @@ const fs = require("fs");
 const path = require("path");
 const bcryptjs = require("bcryptjs");
 const { validationResult } = require("express-validator");
+const db = require('../database/models');
+const sequelize = db.sequelize;
+
+const Users = db.User;
 
 let categorias = [
   {
@@ -119,10 +123,8 @@ const usersController = {
         old: req.body,
       });
     }
-    const users = findAll();
-
-    const newUser = {
-      id: users.length + 1,
+    
+    Users.create({
       nombre: req.body.nombre,
       apellido: req.body.apellido,
       fechaDeNacimiento: req.body.fechaDeNacimiento,
@@ -130,55 +132,104 @@ const usersController = {
       telefono: req.body.telefono,
       email: req.body.email,
       image: req.file.filename,
-      password: bcryptjs.hashSync(req.body.password, 10),
-    };
+      password: bcryptjs.hashSync(req.body.password, 10)
+  }) 
+  .then(function(){
+      res.redirect('/users/login');
+  });
+    
+    // En caso de no usar base de datos: 
+  
+    // const newUser = {
+    //   id: users.length + 1,
+    //   nombre: req.body.nombre,
+    //   apellido: req.body.apellido,
+    //   fechaDeNacimiento: req.body.fechaDeNacimiento,
+    //   genero: req.body.genero,
+    //   telefono: req.body.telefono,
+    //   email: req.body.email,
+    //   image: req.file.filename,
+    //   password: bcryptjs.hashSync(req.body.password, 10),
+    // };
 
-    users.push(newUser);
+    // users.push(newUser);
 
-    writeFile(users);
+    // writeFile(users);
 
-    res.redirect("/users/login");
+    // res.redirect("/users/login");
+    
   },
-  editView: (req, res) => {
-    const data = findAll();
-    const userFound = data.find(function (user) {
 
-      return user.id == req.session.usuarioLogueado.id
-    });
-    res.render("../views/users/profileView", {
-      user: userFound,
-    });
+  editView: (req, res) => {
+    let userId = req.session.usuarioLogueado.id;
+    Users.findByPk(userId)
+    .then(function(user){
+        res.render('../views/users/profileView', {user: userId});
+    })
+    // En caso de no usar base de datos: 
+    // const data = findAll();
+    // const userFound = data.find(function (user) {
+
+    //   return user.id == req.session.usuarioLogueado.id
+    // });
+    // res.render("../views/users/profileView", {
+    //   user: userFound,
+    // });
   },
   edit: (req, res) => {
-    const data = findAll();
-    const userFound = data.find(function (user) {
+    let userId = req.session.usuarioLogueado.id;
+    Users.findByPk(userId)
+    .then(function(user){
+        res.render('../views/users/profileEdition', {user: userId});
+    })
+    // En caso de no usar base de datos: 
+    // const data = findAll();
+    // const userFound = data.find(function (user) {
 
-      return user.id == req.session.usuarioLogueado.id
-    });
-    res.render("../views/users/profileEdition", {
-      user: userFound,
-    });
+    //   return user.id == req.session.usuarioLogueado.id
+    // });
+    // res.render("../views/users/profileEdition", {
+    //   user: userFound,
+    // });
   },
 
   update: (req, res) => {
-    
-    const data = findAll();
-    const userFound = data.find(function (user) {
-      return user.id == req.session.usuarioLogueado.id
-    });
-      userFound.image = req.file ? req.file.filename : userFound.image
-      userFound.nombre = req.body.nombre,
-      userFound.apellido = req.body.apellido,
-      userFound.fechaDeNacimiento = req.body.fechaDeNacimiento,
-      userFound.genero = req.body.genero,
-      userFound.telefono = req.body.telefono,
-      userFound.email = req.body.email,
-      userFound.deporteFavorito = req.body.deporteFavorito,
+    let userId = req.params.id;
+        Users.update({
+          nombre: req.body.nombre,
+          apellido: req.body.apellido,
+          fechaDeNacimiento: req.body.fechaDeNacimiento,
+          genero: req.body.genero,
+          telefono: req.body.telefono,
+          email: req.body.email,
+          image: req.file ? req.file.filename : req.body.image
+        },{
+            where:{
+            id: userId
+            } 
+        })
+        .then(function(){
+          res.redirect('/users/profileView')
+        })
+        .catch(error => res.send(error))
+
+    // En caso de no usar base de datos: 
+    // const data = findAll();
+    // const userFound = data.find(function (user) {
+    //   return user.id == req.session.usuarioLogueado.id
+    // });
+    //   userFound.image = req.file ? req.file.filename : userFound.image
+    //   userFound.nombre = req.body.nombre,
+    //   userFound.apellido = req.body.apellido,
+    //   userFound.fechaDeNacimiento = req.body.fechaDeNacimiento,
+    //   userFound.genero = req.body.genero,
+    //   userFound.telefono = req.body.telefono,
+    //   userFound.email = req.body.email,
+    //   userFound.deporteFavorito = req.body.deporteFavorito,
       
+    // writeFile(data);
 
-    writeFile(data);
-
-    res.redirect('/users/profileView');
+    // res.redirect('/users/profileView');
   },
 };
 
